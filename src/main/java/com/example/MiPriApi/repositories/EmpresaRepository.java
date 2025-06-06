90969eae1e3b2dd401879df20c628bc95a51fe30
FL1XEvac <Altamira2004@outlook.com>
2025-06-06 19:50:28 -0300

    Cambios a pedido, ahora no retorna null empleado, sucursal y domicilio

--- src/main/java/com/example/MiPriApi/repositories/EmpresaRepository.java
+++ src/main/java/com/example/MiPriApi/repositories/EmpresaRepository.java
@@ -1,9 +1,8 @@
 package com.example.MiPriApi.repositories;
 
 import com.example.MiPriApi.entities.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+public interface EmpresaRepository extends BaseRepository<Empresa, Long>{
 }
