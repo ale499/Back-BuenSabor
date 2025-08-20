2fff8dee20c799183ea68ee03b11d93f6926177d
ale499 <gimenezalejandro083@gmail.com>
2025-08-19 22:19:41 -0300

    se cambio el acceso al controlador clientcontroller

--- src/main/java/com/example/MiPriApi/controllers/Auth0/ClientController.java
+++ src/main/java/com/example/MiPriApi/controllers/Auth0/ClientController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Set;
 
+@PreAuthorize("hasAuthority('client')")
 @RestController
 @RequestMapping(path = "/api/client", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ClientController {
